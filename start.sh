064f1fb96d93a33f834523dbde78a33aaf7b6835
James Lewis <James.Lewis@certussolutions.com>
2025-02-17 16:30:59 +1100

    try setting nproc-per-node to 1

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 git pull
 pkill python3
-nohup torchrun --standalone --nnodes=1 --nproc-per-node=4 cli_autonomous.py --model THUDM/cogvlm2-llama3-chat-19B &
+nohup torchrun --standalone --nnodes=1 --nproc-per-node=1 cli_autonomous.py --model THUDM/cogvlm2-llama3-chat-19B &
 nohup python3 ./message_publisher.py &
 tail -f ./nohup.out
