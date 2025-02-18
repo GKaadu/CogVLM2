ebe2636fe136436266ad4b1be00494d63b347fdb
James Lewis <James.Lewis@certussolutions.com>
2025-02-18 17:01:48 +1100

    updated start.sh for use as a service

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
-git pull
+pwd
+cd /home/ubuntu/CogVLM2
+# git pull
 pkill python3
-nohup torchrun --standalone --nnodes=1 --nproc-per-node=1 cli_autonomous.py --model THUDM/cogvlm2-llama3-chat-19B &
-nohup python3 ./message_publisher.py &
-tail -f ./nohup.out
+nohup /home/ubuntu/.venv/bin/torchrun --standalone --nnodes=1 --nproc-per-node=1 /home/ubuntu/CogVLM2/cli_autonomous.py --model THUDM/cogvlm2-llama3-chat-19B &
+nohup /home/ubuntu/.venv/bin/python3 /home/ubuntu/CogVLM2/message_publisher.py &
+tail -f /home/ubuntu/CogVLM2/nohup.out
